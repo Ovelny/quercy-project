b1e008a8dcb6a2329e03cc74a3e9bd3e06dcbffb
Claire Lapointe <clapointe111@gmail.com>
2017-02-20 17:51:05 +0100

    démo-test directive

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-angular.module('quercy', [])
 
-.directive("customButton", function() {
-  return {
-    template:"<h1>test</h1>"
-  };
-});
+angular.module('quercy', []);
+
---
+++ directives/test.directive.js
@@ -0,0 +1,7 @@
+
+angular.module('quercy')
+.directive("customButton", function() {
+  return {
+    template:"<h1>test</h1>"
+  };
+});
