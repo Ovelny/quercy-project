5b8c4f9f851dbc18e1c361cca449f3b84c1af616
Claire Lapointe <clapointe111@gmail.com>
2017-02-27 11:24:29 +0100

    filtres sur liste properties

--- API/urls.py
+++ API/urls.py
@@ -8,7 +8,7 @@ from . import views
 # router.register crée automatiquement l'adresse items/{id}/ en plus de items/
 
 router = routers.DefaultRouter()
-router.register(r'properties', views.Property_Viewset) 
+# router.register(r'properties', views.Property_Viewset) 
 router.register(r'property_types', views.Property_Type_Viewset)
 router.register(r'heating_types', views.Heating_Type_Viewset)
 router.register(r'kitchen_types', views.Kitchen_Type_Viewset)
@@ -19,4 +19,6 @@ router.register(r'company_info', views.Company_Info_Viewset)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^properties/$', views.Property_List.as_view()),
+    url(r'^properties/(?P<pk>[0-9]+)/$', views.Property_Detail.as_view()),
 ]
--- projectQuercy/settings.py
+++ projectQuercy/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    # 'books',
     'rest_framework',
     'API',
 ]
