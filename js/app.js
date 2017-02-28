51045b6917f28eb555f7d891f7c73f12b08048b5
Ovelny <this.is.ovelny@gmail.com>
2017-02-28 08:58:55 +0100

    routes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 (function () {
-    var app = angular.module('quercy-front', ['front-directives']);
+    var app = angular.module('quercy-front', ['front-directives', 'ngRoute']);
 
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {})
+            .when('/presentation', {})
+            .when('/vente', {})
+            .when('/location', {})
+            .when('/coups-de-coeur', {})
+            .when('/estimation', {})
+            .when('/contact', {})
+
+
+
+    }]);
 
 
 
