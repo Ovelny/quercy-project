99282eb6afdbdfecee35e14522b31d74b794e9d3
Claire Lapointe <clapointe111@gmail.com>
2017-03-05 09:13:18 +0100

    amélioration fonctionnement menu

--- js/back-office/directives/directives.js
+++ js/back-office/directives/directives.js
@@ -1,12 +1,16 @@
 (function () {
     var app = angular.module('quercy-back');
 
-    app.directive('sidemenu', function () {
+    app.directive('sidemenu', function ($location) {
         return {
             restrict: 'E',
-            templateUrl: 'js/back-office/partials/sidemenu.html'
+            templateUrl: 'js/back-office/partials/sidemenu.html',
+            controller: function ($scope) {
+                $scope.navClass = function (page) {
+                    var currentRoute = $location.path().substring(1) || '';
+                    return page === currentRoute ? 'active' : '';
+                };
+            }
         }
-
     });
-
 })();
