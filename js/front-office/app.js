0a50127cc7a28f788a6636958aacd17c9b1289a3
Claire Lapointe <clapointe111@gmail.com>
2017-04-05 23:26:12 +0200

    correction merge

--- js/front-office/app.js
+++ js/front-office/app.js
@@ -4,16 +4,11 @@
     app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/', { templateUrl: 'js/front-office/views/welcome.html' })
-<<<<<<< HEAD
-            .when('/presentation', { templateUrl: 'js/front-office/views/presentation.html' })
             .when('/recherche', { templateUrl: 'js/front-office/views/recherche.html' })
-            .when('/liste/:type', {
-=======
             .when('/presentation', { 
                 templateUrl: 'js/front-office/views/presentation.html',
                 controller: 'presentationController' })
             .when('/liste/:typeliste', { 
->>>>>>> 5ee460ad6ecf5bd01531e0b8f0e0f8969306ff3c
                 templateUrl: 'js/front-office/views/listebiens.html',
                 controller: 'listeController'
             })
@@ -24,11 +19,6 @@
             .when('/contact', { templateUrl: 'js/front-office/views/contact.html' })
             .otherwise({ redirectTo: '/' });
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 5ee460ad6ecf5bd01531e0b8f0e0f8969306ff3c
     }]).run(['$rootScope',
         function ($rootScope) {
             $rootScope.language = "fr";
