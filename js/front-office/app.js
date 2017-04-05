4211a8a6b3b2bfabcbfb379f602addb13ffd4548
Claire Lapointe <clapointe111@gmail.com>
2017-04-05 22:01:24 +0200

    modifs routes

--- js/front-office/app.js
+++ js/front-office/app.js
@@ -5,9 +5,9 @@
         $routeProvider
             .when('/', { templateUrl: 'js/front-office/views/welcome.html' })
             .when('/presentation', { templateUrl: 'js/front-office/views/presentation.html' })
-            .when('/vente', { templateUrl: 'js/front-office/views/vente.html' })
-            .when('/location', { templateUrl: 'js/front-office/views/location.html' })
-            .when('/coups-de-coeur', { templateUrl: 'js/front-office/views/coups-de-coeur.html' })
+            .when('/liste/:type', { 
+                templateUrl: 'js/front-office/views/listebiens.html',
+                controller: 'listeController' })
             .when('/estimation', { 
                 templateUrl: 'js/front-office/views/estimation.html',
                 controller: 'estimationController' })
